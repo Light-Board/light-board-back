fb39fcc773674f6e14eb3ef22d2c893578df4ba9
Nuung <qlgks1@naver.com>
2021-09-12 00:06:22 +0900

    remove unused class import and log

--- src/main/java/com/lb/lightboard/repository/BoardFrameRepository.java
+++ src/main/java/com/lb/lightboard/repository/BoardFrameRepository.java
@@ -4,11 +4,7 @@ import com.lb.lightboard.model.entity.BoardFrame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
-import java.util.List;
-
 @Repository
 public interface BoardFrameRepository extends JpaRepository<BoardFrame, Long> {
 
-//	List<BoardFrame> findAll(Pageable pageable);
 }
