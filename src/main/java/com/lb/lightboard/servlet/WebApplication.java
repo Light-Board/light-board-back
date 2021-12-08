e7e6fb446453c6c91152fbcfb7952e32cd9dfb79
SeyoungJeon <seyoung0254@hanmail.net>
2021-12-08 21:16:36 +0900

    Spring Security Filter 설정 추가

--- src/main/java/com/lb/lightboard/servlet/WebApplication.java
+++ src/main/java/com/lb/lightboard/servlet/WebApplication.java
@@ -1,5 +1,8 @@
 package com.lb.lightboard.servlet;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,6 +13,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +36,9 @@ public class WebApplication implements WebApplicationInitializer {
 		setServletDispatcherContext(context, servletContext);
 
 		// Filter 설정
+		FilterRegistration securityFilter = servletContext.addFilter("springSecurityFilterChain",
+			DelegatingFilterProxy.class);
+		securityFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), true, "/*");
 		setCharacterEncodingFilter(servletContext);
 	}
 
